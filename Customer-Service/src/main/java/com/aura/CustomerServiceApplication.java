5b421168551786199b3ca61ea0dd5a3c3b23d33c
salah <akilsalaheddine203@gmail.com>
2025-02-18 22:14:25 +0100

    service structure

--- Customer-Service/src/main/java/com/aura/CustomerServiceApplication.java
+++ Customer-Service/src/main/java/com/aura/CustomerServiceApplication.java
@@ -1,13 +1,13 @@
-package com.aura.Account_Service;
+package com.aura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountServiceApplication {
+public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountServiceApplication.class, args);
+		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
 }
